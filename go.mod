a912209fc98c8af2f87fce2a088fa9d278034528
optimus-alfaomega <a-and-o-technologies@outlook.com>
2025-09-08 09:18:18 -0500

    Primer commit en go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Optimus-alfaomega/mi-primer-proyecto-go
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hola, estas corriendo tu primer proyecto Go en Vs Code")
+}
